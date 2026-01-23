4198eae50c9d7c748a62ba78b3a53199c3065898
Yuri <ndcyuri@gmail.com>
2026-01-23 16:26:02 -0300

    feat: validation added

--- Focus-tracker/src/main/java/br/com/yuri/Focus/tracker/data/dto/SessionRequestDTO.java
+++ Focus-tracker/src/main/java/br/com/yuri/Focus/tracker/data/dto/SessionRequestDTO.java
@@ -1,15 +1,22 @@
 package br.com.yuri.Focus.tracker.data.dto;
 
 import br.com.yuri.Focus.tracker.model.Category;
-
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
 public class SessionRequestDTO {
 
+    @NotBlank
     private String title;
+
     private LocalDateTime beginDate;
+
+    @Future
     private LocalDateTime endDate;
+
+    @NotBlank
     private Category category;
 
     public SessionRequestDTO(String title, LocalDateTime beginDate, LocalDateTime endDate, Category category) {
